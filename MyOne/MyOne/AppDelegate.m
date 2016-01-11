d77a15e9530b42e77017129b12b2c046dda9d7f1
tangqi92 <imtangqi@gmail.com>
2016-01-11 19:56:35 +0800

    Add the function of share

--- MyOne/MyOne/AppDelegate.m
+++ MyOne/MyOne/AppDelegate.m
@@ -15,6 +15,17 @@
 #import "DSNavigationBar.h"
 #import "AppConfigure.h"
 #import "TopWindow.h"
+// ShareSDK头文件
+#import <ShareSDK/ShareSDK.h>
+#import <ShareSDKConnector/ShareSDKConnector.h>
+// 腾讯开放平台（对应QQ和QQ空间）SDK头文件
+#import <TencentOpenAPI/TencentOAuth.h>
+#import <TencentOpenAPI/QQApiInterface.h>
+// 微信SDK头文件
+#import "WXApi.h"
+// 新浪微博SDK头文件
+// 新浪微博SDK需要在项目Build Settings中的Other Linker Flags添加"-ObjC"
+#import "WeiboSDK.h"
 
 @interface AppDelegate ()
 
@@ -24,6 +35,63 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    /**
+     *  设置ShareSDK的appKey，如果尚未在ShareSDK官网注册过App，请移步到http://mob.com/login 登录后台进行应用注册，
+     *  在将生成的AppKey传入到此方法中。
+     *  方法中的第二个参数用于指定要使用哪些社交平台，以数组形式传入。第三个参数为需要连接社交平台SDK时触发，
+     *  在此事件中写入连接代码。第四个参数则为配置本地社交平台时触发，根据返回的平台类型来配置平台信息。
+     *  如果您使用的时服务端托管平台信息时，第二、四项参数可以传入nil，第三项参数则根据服务端托管平台来决定要连接的社交SDK。
+     */
+    [ShareSDK registerApp:@"iosv1101"
+          activePlatforms:@[
+                            @(SSDKPlatformTypeSinaWeibo),
+                            @(SSDKPlatformTypeCopy),
+                            @(SSDKPlatformTypeWechat),
+                            @(SSDKPlatformTypeQQ)
+                            ]
+                 onImport:^(SSDKPlatformType platformType) {
+                     
+                     switch (platformType)
+                     {
+                         case SSDKPlatformTypeWechat:
+                             [ShareSDKConnector connectWeChat:[WXApi class]];
+                             break;
+                         case SSDKPlatformTypeQQ:
+                             [ShareSDKConnector connectQQ:[QQApiInterface class]
+                                        tencentOAuthClass:[TencentOAuth class]];
+                             break;
+                         case SSDKPlatformTypeSinaWeibo:
+                             [ShareSDKConnector connectWeibo:[WeiboSDK class]];
+                             break;
+                         default:
+                             break;
+                     }
+                     
+                 }
+          onConfiguration:^(SSDKPlatformType platformType, NSMutableDictionary *appInfo) {
+              
+              switch (platformType)
+              {
+                  case SSDKPlatformTypeSinaWeibo:
+                      //设置新浪微博应用信息,其中authType设置为使用SSO＋Web形式授权
+                      [appInfo SSDKSetupSinaWeiboByAppKey:@"568898243"
+                                                appSecret:@"38a4f8204cc784f81f9f0daaf31e02e3"
+                                              redirectUri:@"http://itangqi.me"
+                                                 authType:SSDKAuthTypeBoth];
+                      break;
+                  case SSDKPlatformTypeWechat:
+                      [appInfo SSDKSetupWeChatByAppId:@"wx4868b35061f87885"
+                                            appSecret:@"64020361b8ec4c99936c0e3999a9f249"];
+                      break;
+                  case SSDKPlatformTypeQQ:
+                      [appInfo SSDKSetupQQByAppId:@"100371282"
+                                           appKey:@"aed9b0303e3ed1e27bae87c33761161d"
+                                         authType:SSDKAuthTypeBoth];
+                      break;
+                  default:
+                      break;
+              }
+          }];
 	// Override point for customization after application launch.
 	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 	
--- MyOne/MyOne/ViewControllers/BaseViewController/BaseViewController.m
+++ MyOne/MyOne/ViewControllers/BaseViewController/BaseViewController.m
@@ -9,13 +9,17 @@
 #import "BaseViewController.h"
 #import <MBProgressHUD/MBProgressHUD.h>
 #import "DSNavigationBar.h"
-
+#import <ShareSDK/ShareSDK.h>
+#import <ShareSDKUI/ShareSDK+SSUI.h>
 @interface BaseViewController () <MBProgressHUDDelegate>
+
+@property (nonatomic, strong) UIView *panelView;
+@property (nonatomic, strong) UIActivityIndicatorView *loadingView;
 
 @end
 
 @implementation BaseViewController {
-	MBProgressHUD *HUD;
+    MBProgressHUD *HUD;
 }
 
 #pragma mark - View Lifecycle
@@ -23,11 +27,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-	self.view.backgroundColor = [UIColor whiteColor];
-	// 设置夜间模式背景色
-	self.view.nightBackgroundColor = NightBGViewColor;
-	//设置标题栏不能覆盖下面viewcontroller的内容
-	self.edgesForExtendedLayout = UIRectEdgeNone;
+    self.view.backgroundColor = [UIColor whiteColor];
+    // 设置夜间模式背景色
+    self.view.nightBackgroundColor = NightBGViewColor;
+    // 设置标题栏不能覆盖下面viewcontroller的内容
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    //加载等待视图
+    self.panelView = [[UIView alloc] initWithFrame:self.view.bounds];
+    self.panelView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    self.panelView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.3];
+    
+    self.loadingView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    self.loadingView.frame = CGRectMake((self.view.frame.size.width - self.loadingView.frame.size.width) / 2, (self.view.frame.size.height - self.loadingView.frame.size.height) / 2, self.loadingView.frame.size.width, self.loadingView.frame.size.height);
+    self.loadingView.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
+    [self.panelView addSubview:self.loadingView];
+    
 }
 
 #pragma mark - Lifecycle
@@ -40,171 +54,286 @@
 #pragma mark - Public
 
 - (void)setUpNavigationBarShowRightBarButtonItem:(BOOL)show {
-	// 显示导航栏上的《一个》图标
-	self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"navLogo"]];
-	if (show) {
-		// 初始化导航栏右侧的分享按钮
-		UIButton *shareButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 22, 22)];
-		[shareButton setBackgroundImage:[UIImage imageNamed:@"nav_share_btn_normal"] forState:UIControlStateNormal];
-		[shareButton setBackgroundImage:[UIImage imageNamed:@"nav_share_btn_highlighted"] forState:UIControlStateHighlighted];
-		
-		UIBarButtonItem *shareItem = [[UIBarButtonItem alloc] initWithCustomView:shareButton];
-		
-		self.navigationItem.rightBarButtonItem = shareItem;
-	}
+    // 显示导航栏上的《一个》图标
+    self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"navLogo"]];
+    if (show) {
+        // 初始化导航栏右侧的分享按钮
+        UIButton *shareButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 22, 22)];
+        [shareButton setBackgroundImage:[UIImage imageNamed:@"nav_share_btn_normal"] forState:UIControlStateNormal];
+        [shareButton setBackgroundImage:[UIImage imageNamed:@"nav_share_btn_highlighted"] forState:UIControlStateHighlighted];
+        [shareButton addTarget:self action:@selector(shareToSocial) forControlEvents:UIControlEventTouchUpInside];
+        UIBarButtonItem *shareItem = [[UIBarButtonItem alloc] initWithCustomView:shareButton];
+        
+        self.navigationItem.rightBarButtonItem = shareItem;
+    }
 }
 
 - (void)dontShowBackButtonTitle {
-	[[UIBarButtonItem appearance] setBackButtonTitlePositionAdjustment:UIOffsetMake(0, -60) forBarMetrics:UIBarMetricsDefault];
+    [[UIBarButtonItem appearance] setBackButtonTitlePositionAdjustment:UIOffsetMake(0, -60) forBarMetrics:UIBarMetricsDefault];
 }
 
 #pragma mark - Private
 
 - (UIImage *)imageWithColor:(UIColor *)color {
-	CGRect rect = CGRectMake(0, 0, 1, 1);
-	UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
-	[color setFill];
-	UIRectFill(rect);
-	
-	UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-	UIGraphicsEndImageContext();
-	
-	return image;
+    CGRect rect = CGRectMake(0, 0, 1, 1);
+    UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
+    [color setFill];
+    UIRectFill(rect);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
 }
 
 #pragma mark - Touch Events
 
 - (void)share {
-	
+    
 }
 
 #pragma mark - MBProgressHUD
 
 - (void)showHUDWaitingWhileExecuting:(SEL)method {
-	// The hud will dispable all input on the view (use the higest view possible in the view hierarchy)
-	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
-	[self.navigationController.view addSubview:HUD];
-	
-	HUD.color = [UIColor colorWithRed:100 / 255.0 green:100 / 255.0 blue:100 / 255.0 alpha:0.9];
-	
-	// Regiser for HUD callbacks so we can remove it from the window at the right time
-	HUD.delegate = self;
-	
-	// Show the HUD while the provided method executes in a new thread
-	[HUD showWhileExecuting:method onTarget:self withObject:nil animated:YES];
+    // The hud will dispable all input on the view (use the higest view possible in the view hierarchy)
+    HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+    [self.navigationController.view addSubview:HUD];
+    
+    HUD.color = [UIColor colorWithRed:100 / 255.0 green:100 / 255.0 blue:100 / 255.0 alpha:0.9];
+    
+    // Regiser for HUD callbacks so we can remove it from the window at the right time
+    HUD.delegate = self;
+    
+    // Show the HUD while the provided method executes in a new thread
+    [HUD showWhileExecuting:method onTarget:self withObject:nil animated:YES];
 }
 
 - (void)showHUDWithText:(NSString *)text delay:(NSTimeInterval)delay {
-	if (!HUD.isHidden) {
-		[HUD hide:NO];
-	}
-	HUD = [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:YES];
-	// Configure for text only and offset down
-	HUD.mode = MBProgressHUDModeText;
-	HUD.labelText = text;
-	HUD.margin = 10.f;
-	HUD.removeFromSuperViewOnHide = YES;
-	// Regiser for HUD callbacks so we can remove it from the window at the right time
-	HUD.delegate = self;
-	[HUD hide:YES afterDelay:delay];
+    if (!HUD.isHidden) {
+        [HUD hide:NO];
+    }
+    HUD = [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:YES];
+    // Configure for text only and offset down
+    HUD.mode = MBProgressHUDModeText;
+    HUD.labelText = text;
+    HUD.margin = 10.f;
+    HUD.removeFromSuperViewOnHide = YES;
+    // Regiser for HUD callbacks so we can remove it from the window at the right time
+    HUD.delegate = self;
+    [HUD hide:YES afterDelay:delay];
 }
 
 - (void)showHUDDone {
-	[self showHUDDoneWithText:@"Done"];
+    [self showHUDDoneWithText:@"Done"];
 }
 
 - (void)showHUDDoneWithText:(NSString *)text {
-	if (!HUD.isHidden) {
-		[HUD hide:NO];
-	}
-	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
-	[self.navigationController.view addSubview:HUD];
-	HUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"common_icon_right"]];
-	HUD.mode = MBProgressHUDModeCustomView;
-	HUD.labelText = text;
-	// Regiser for HUD callbacks so we can remove it from the window at the right time
-	HUD.delegate = self;
-	[HUD show:YES];
-	[HUD hide:YES afterDelay:HUD_DELAY];
+    if (!HUD.isHidden) {
+        [HUD hide:NO];
+    }
+    HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+    [self.navigationController.view addSubview:HUD];
+    HUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"common_icon_right"]];
+    HUD.mode = MBProgressHUDModeCustomView;
+    HUD.labelText = text;
+    // Regiser for HUD callbacks so we can remove it from the window at the right time
+    HUD.delegate = self;
+    [HUD show:YES];
+    [HUD hide:YES afterDelay:HUD_DELAY];
 }
 
 - (void)showHUDErrorWithText:(NSString *)text {
-	if (!HUD.isHidden) {
-		[HUD hide:NO];
-	}
-	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
-	[self.navigationController.view addSubview:HUD];
-	HUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"common_icon_error"]];
-	HUD.mode = MBProgressHUDModeCustomView;
-	HUD.labelText = text;
-	// Regiser for HUD callbacks so we can remove it from the window at the right time
-	HUD.delegate = self;
-	[HUD show:YES];
-	[HUD hide:YES afterDelay:HUD_DELAY];
+    if (!HUD.isHidden) {
+        [HUD hide:NO];
+    }
+    HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+    [self.navigationController.view addSubview:HUD];
+    HUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"common_icon_error"]];
+    HUD.mode = MBProgressHUDModeCustomView;
+    HUD.labelText = text;
+    // Regiser for HUD callbacks so we can remove it from the window at the right time
+    HUD.delegate = self;
+    [HUD show:YES];
+    [HUD hide:YES afterDelay:HUD_DELAY];
 }
 
 - (void)showHUDNetError {
-	[self showHUDErrorWithText:BadNetwork];
+    [self showHUDErrorWithText:BadNetwork];
 }
 
 - (void)showHUDServerError {
-	[self showHUDErrorWithText:@"Server Error"];
+    [self showHUDErrorWithText:@"Server Error"];
 }
 
 - (void)showWithLabelText:(NSString *)showText executing:(SEL)method {
-	if (!HUD.isHidden) {
-		[HUD hide:NO];
-	}
-	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
-	[self.navigationController.view addSubview:HUD];
-	HUD.labelText = showText;
-	// Regiser for HUD callbacks so we can remove it from the window at the right time
-	HUD.delegate = self;
-	[HUD showWhileExecuting:method onTarget:self withObject:nil animated:YES];
+    if (!HUD.isHidden) {
+        [HUD hide:NO];
+    }
+    HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+    [self.navigationController.view addSubview:HUD];
+    HUD.labelText = showText;
+    // Regiser for HUD callbacks so we can remove it from the window at the right time
+    HUD.delegate = self;
+    [HUD showWhileExecuting:method onTarget:self withObject:nil animated:YES];
 }
 
 - (void)showHUDWithText:(NSString *)text {
-	[self hideHud];
-	HUD = [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:YES];
-	// Configure for text only and offset down
-	HUD.mode = MBProgressHUDModeIndeterminate;
-	HUD.labelText = text;
-	HUD.margin = 10.f;
-	// Regiser for HUD callbacks so we can remove it from the window at the right time
-	HUD.delegate = self;
-	HUD.removeFromSuperViewOnHide = YES;
+    [self hideHud];
+    HUD = [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:YES];
+    // Configure for text only and offset down
+    HUD.mode = MBProgressHUDModeIndeterminate;
+    HUD.labelText = text;
+    HUD.margin = 10.f;
+    // Regiser for HUD callbacks so we can remove it from the window at the right time
+    HUD.delegate = self;
+    HUD.removeFromSuperViewOnHide = YES;
 }
 
 - (void)processServerErrorWithCode:(NSInteger)code andErrorMsg:(NSString *)msg {
-	if (code == 500) {
-		[self showHUDServerError];
-	} else {
-		[self showHUDDoneWithText:msg];
-	}
+    if (code == 500) {
+        [self showHUDServerError];
+    } else {
+        [self showHUDDoneWithText:msg];
+    }
 }
 
 - (void)hideHud {
-	if (!HUD.isHidden) {
-		[HUD hide:NO];
-	}
+    if (!HUD.isHidden) {
+        [HUD hide:NO];
+    }
 }
 
 #pragma mark - MBProgressHUDDelegate
 
 - (void)hudWasHidden:(MBProgressHUD *)hud {
-	if (self.hudWasHidden) {
-		self.hudWasHidden();
-	}
+    if (self.hudWasHidden) {
+        self.hudWasHidden();
+    }
 }
 
 /*
-#pragma mark - Navigation
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+#pragma mark - ShareAction
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)shareToSocial
+{
+    /**
+     * 在简单分享中，只要设置共有分享参数即可分享到任意的社交平台
+     **/
+    __weak BaseViewController *theController = self;
+    
+    //1、创建分享参数（必要）
+    NSMutableDictionary *shareParams = [NSMutableDictionary dictionary];
+    NSArray* imageArray = @[[UIImage imageNamed:@"navLogo.png"]];
+    [shareParams SSDKSetupShareParamsByText:@"分享内容"
+                                     images:imageArray
+                                        url:[NSURL URLWithString:@"http://itangqi.me"]
+                                      title:@"分享标题"
+                                       type:SSDKContentTypeAuto];
+    
+    //2、分享
+    [ShareSDK showShareActionSheet:nil
+                             items:nil
+                       shareParams:shareParams
+               onShareStateChanged:^(SSDKResponseState state, SSDKPlatformType platformType, NSDictionary *userData, SSDKContentEntity *contentEntity, NSError *error, BOOL end) {
+                   
+                   switch (state) {
+                           
+                       case SSDKResponseStateBegin:
+                       {
+                           [theController showLoadingView:YES];
+                           break;
+                       }
+                       case SSDKResponseStateSuccess:
+                       {
+                           UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"分享成功"
+                                                                               message:nil
+                                                                              delegate:nil
+                                                                     cancelButtonTitle:@"确定"
+                                                                     otherButtonTitles:nil];
+                           [alertView show];
+                           break;
+                       }
+                       case SSDKResponseStateFail:
+                       {
+                           if (platformType == SSDKPlatformTypeSMS && [error code] == 201)
+                           {
+                               UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享失败"
+                                                                               message:@"失败原因可能是：1、短信应用没有设置帐号；2、设备不支持短信应用；3、短信应用在iOS 7以上才能发送带附件的短信。"
+                                                                              delegate:nil
+                                                                     cancelButtonTitle:@"OK"
+                                                                     otherButtonTitles:nil, nil];
+                               [alert show];
+                               break;
+                           }
+                           else if(platformType == SSDKPlatformTypeMail && [error code] == 201)
+                           {
+                               UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享失败"
+                                                                               message:@"失败原因可能是：1、邮件应用没有设置帐号；2、设备不支持邮件应用；"
+                                                                              delegate:nil
+                                                                     cancelButtonTitle:@"OK"
+                                                                     otherButtonTitles:nil, nil];
+                               [alert show];
+                               break;
+                           }
+                           else
+                           {
+                               UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享失败"
+                                                                               message:[NSString stringWithFormat:@"%@",error]
+                                                                              delegate:nil
+                                                                     cancelButtonTitle:@"OK"
+                                                                     otherButtonTitles:nil, nil];
+                               [alert show];
+                               break;
+                           }
+                           break;
+                       }
+                       case SSDKResponseStateCancel:
+                       {
+                           UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"分享已取消"
+                                                                               message:nil
+                                                                              delegate:nil
+                                                                     cancelButtonTitle:@"确定"
+                                                                     otherButtonTitles:nil];
+                           [alertView show];
+                           break;
+                       }
+                       default:
+                           break;
+                   }
+                   
+                   if (state != SSDKResponseStateBegin)
+                   {
+                       [theController showLoadingView:NO];
+                       
+                   }
+                   
+               }];
 }
-*/
+
+/**
+ *  显示加载动画
+ *
+ *  @param flag YES 显示，NO 不显示
+ */
+- (void)showLoadingView:(BOOL)flag
+{
+    if (flag)
+    {
+        [self.view addSubview:self.panelView];
+        [self.loadingView startAnimating];
+    }
+    else
+    {
+        [self.panelView removeFromSuperview];
+    }
+}
 
 @end
